5e8304b8f0e947777781f01c93a35830936ca37a
Pham Thanh Hien <hien9388@donga.edu.vn>
2025-08-25 21:43:46 +0700

    create base constructor for frontend and backend

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI as string,
+      {
+        dbName: 'instagram_clone'
+      }
+    )
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
